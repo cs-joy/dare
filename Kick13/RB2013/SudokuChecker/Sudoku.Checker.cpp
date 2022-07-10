707f36be5459477b79fc3878449b5f67b7906db5
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-07-10 09:26:49 +0600

    init

---
+++ Kick13/RB2013/SudokuChecker/Sudoku.Checker.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Sudoku Checker";
+
+    return 0;
+}
+
+/*
+
+@ source - https://codingcompetitions.withgoogle.com/kickstart/round/0000000000434ad7/00000000004347b3#problem
+
+*/
