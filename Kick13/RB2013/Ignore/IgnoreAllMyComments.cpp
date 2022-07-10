397a290ce7b3df76d9776c61bc9ed4b040c902ba
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-07-10 09:35:51 +0600

    init

---
+++ Kick13/RB2013/Ignore/IgnoreAllMyComments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Ignore all of my comments";
+
+    return 0;
+}
+
+/*
+
+@ source - https://codingcompetitions.withgoogle.com/kickstart/round/0000000000434ad7/0000000000434dfc
+
+*/
