ab425d2474b3d3bcf036ec113589d2af0999f0f1
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-07-21 06:32:13 +0600

    initial commit

---
+++ Kick15/PR/captainHammer.java
@@ -0,0 +1,7 @@
+package Kick15.PR;
+
+public class captainHammer {
+    public static void main(String[] args) {
+        System.out.println("Capatain Hammer!");
+    }
+}
--- Kick15/PR/moist.java
+++ Kick15/PR/moist.java
@@ -1,3 +1,5 @@
+package Kick15.PR;
+
 class Moist {
     public static void main(String[] args) {
         System.out.println("Moist!"); 
