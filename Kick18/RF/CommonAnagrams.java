487f6dcea77eff5e68af510dcf215f6155ce41d3
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-07-24 08:15:33 +0600

    initial commit

---
+++ Kick18/RF/CommonAnagrams.java
@@ -0,0 +1,7 @@
+package Kick18.RF;
+
+public class CommonAnagrams {
+    public static void main(String[] args) {
+        System.out.println("Common Anagrams");
+    }
+}
---
+++ Kick18/RF/PalindromeSequence.java
@@ -0,0 +1,7 @@
+package Kick18.RF;
+
+public class PalindromeSequence {
+    public static void main(String[] args) {
+        System.out.println("Palindrome Sequence");
+    }
+}
